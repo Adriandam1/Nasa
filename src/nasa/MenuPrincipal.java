808f328e1cd38d7a9f0227bdc403e80f666e46bb
Usuario <Usuario@DESKTOP-FNJ8BDH>
2024-04-23 12:54:31 +0200

    empiezo pantalla principal

--- src/nasa/MenuPrincipal.java
+++ src/nasa/MenuPrincipal.java
@@ -4,15 +4,44 @@
  */
 package nasa;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
  * @author Adrian
  */
 public class MenuPrincipal  extends JFrame{
+    public MenuPrincipal(){
+        JPanel menu1 = new JPanel();
+        menu1.setLayout(null);
+   
+        JButton botonMisiones = new JButton("MENU MISIONES");
+        botonMisiones.setBounds(350,100,150,20);
     
-    
-    
-    
+        
+        
+        // creo mi objeto con clase Toolkit
+        Toolkit mipantalla= Toolkit.getDefaultToolkit();
+        // Uso metodo "getScreenSize" para sacar el tamaño de la pantalla(nos devuelve un objeto de tipo "Dimension")
+        Dimension tamanoPantalla= mipantalla.getScreenSize();
+        // "Dimension" nos da 2 valores int para altura y ancho. Declaramos variables para obtener los tamaños
+        int alturaPantalla= tamanoPantalla.height;
+        int anchoPantalla= tamanoPantalla.width;
+        
+        // Indicamos tamaño pantalla con los valores de dimension/2 para que nos quede de media pantalla
+        setSize(anchoPantalla/2, alturaPantalla/2);
+        // Coloco la pantalla en /4 /4 para que quede centrado
+        setLocation(anchoPantalla/4,alturaPantalla/4);
+        // Hacemos que no se pueda redimensionar
+        setResizable(false);
+        // Se tiene que poder ver
+        setVisible(true);
+        //Titulo menu principal
+        setTitle("Menu principal de la NASA");
+        // Indicamos que se termine el programa cuando se cierre la pantalla
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }   
 }
